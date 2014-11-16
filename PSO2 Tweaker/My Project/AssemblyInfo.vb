c0b3a7dd8ea1c892bd5f886a3bf46c811a590db8
TheCrimsonDevil <ZeroHeLL@GMail.com>
2014-11-16 06:18:45 -0500

    Changed version number to 3.0.0.0 LIGHTNINGDRAGON EDITION

--- PSO2 Tweaker/My Project/AssemblyInfo.vb
+++ PSO2 Tweaker/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.7.4.6")> 
-<Assembly: AssemblyFileVersion("2.7.4.6")> 
+<Assembly: AssemblyVersion("3.0.0.0")> 
+<Assembly: AssemblyFileVersion("3.0.0.0")> 
