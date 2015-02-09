3276cce86b6c1f4c0316bea46650b258a96fff58
lightningdragon <lightningdragon96@gmail.com>
2015-02-09 04:38:20 -0700

    Refactoring round 2.1.

---
+++ PSO2 Tweaker/My/Program.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Globalization
+Imports System.Threading
+
+Namespace My
+    Public Class Program
+        Public Shared ReadOnly Args As String() = Environment.GetCommandLineArgs()
+        Public Shared ReadOnly StartPath As String = Windows.Forms.Application.StartupPath
+
+        Public Shared MainForm As FrmMain
+        Public Shared HostsFilePath As String
+        Public Shared Pso2RootDir As String
+        Public Shared Pso2WinDir As String
+
+        Public Shared Sub Main()
+            Dim locale = RegKey.GetValue(Of String)(RegKey.Locale)
+
+            If Not String.IsNullOrEmpty(locale) Then
+                Thread.CurrentThread.CurrentUICulture = New CultureInfo(locale)
+                Thread.CurrentThread.CurrentCulture = New CultureInfo(locale)
+            End If
+        End Sub
+    End Class
+End Namespace
