384af6e912b1e78e07b8916a5f76e8dfceea2b2a
TheCrimsonDevil <ZeroHeLL@GMail.com>
2015-02-15 16:38:38 -0500

    version increase

--- PSO2 Tweaker/My Project/AssemblyInfo.vb
+++ PSO2 Tweaker/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.2.9")> 
-<Assembly: AssemblyFileVersion("3.0.2.9")> 
+<Assembly: AssemblyVersion("3.0.3.0")> 
+<Assembly: AssemblyFileVersion("3.0.3.0")> 
