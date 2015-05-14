4ddad21a656d237710599f8ba6f4e95c5fa38678
SonicFreak94 <sonicfreak94@hotmail.com>
2015-05-14 12:28:10 -0600

    A little cleaner.

--- PSO2 Tweaker/frmMain.vb
+++ PSO2 Tweaker/frmMain.vb
@@ -2882,7 +2882,8 @@ Public Class FrmMain
             RegKey.SetValue(Of Boolean)(RegKey.ProxyEnabled, True)
         Catch ex As Exception
             Helper.WriteDebugInfoAndFailed("ERROR - " & ex.Message)
-            If ex.GetType() = GetType(SecurityException) OrElse ex.GetType() = GetType(UnauthorizedAccessException) Then
+            Dim exceptionType As Type = ex.GetType()
+            If exceptionType = GetType(SecurityException) OrElse exceptionType = GetType(UnauthorizedAccessException) Then
                 MsgBox("It seems you've gotten an error while trying to patch your HOSTS file. Please go to the " & Environment.SystemDirectory & "\drivers\etc\ folder, right click on the hosts file, and make sure ""Read Only"" is not checked. Then try again." & vbNewLine & "When you click Okay, the Tweaker will also generate a pastebin of your HOSTS file and what is locking it. Look at the bottom of the pastebin where the 'HOSTS Handle stuff' is.")
                 FrmDiagnostic.Button2.PerformClick()
             End If
